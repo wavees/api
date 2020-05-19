4eb43092b075fc18b08115c1bab6ac7f04dc6e4d
Shyrshling <dj.gleb@list.ru>
2020-05-19 09:55:14 +0300

    Changed disclaimer route

--- routes/accounts/index.js
+++ routes/accounts/index.js
@@ -10,7 +10,9 @@ const helpers = {
   updateToken: require('../../helpers/tokens/update'),
 
   randomizer: require('../../helpers/randomizer'),
-  removeFromArray: require('../../helpers/removeFromArray')
+  removeFromArray: require('../../helpers/removeFromArray'),
+
+  getStore: require('../../helpers/stores/get')
 };
 
 // 
@@ -203,6 +205,60 @@ router.get('/:token', (req, res) => {
   // .catch((error) => {
   //   res.status(500).end({ error: "ServerError", message: error })
   // });
+});
+
+
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+router.get('/:token/applications/:origin', (req, res) => {
+  let token  = req.params.token;
+  let origin = req.params.origin;
+
+  helpers.getToken(token)
+  .then((data) => {
+    if (data.data.type == "user") {
+      // Let's get that registrat...
+      let query = {
+        $$findOne: true,
+
+        type: "disclaimer",
+        registrat: {
+          url: origin
+        }
+      };
+      helpers.getStore(token, query)
+      .then((response) => {
+        let data = response;
+
+        if (data.error == "404") {
+          res.end(JSON.stringify({}));
+        } else {
+          let response = {
+            agreed: true,
+
+            registrat: data.registrat
+          };
+
+          res.end(JSON.stringify(response));
+        }
+      }).catch((error) => {
+        res.status(error == "InvalidToken" ? 400 : 500).end(JSON.stringify({ error: error }));
+      });
+    } else {
+      res.status(400).end(JSON.stringify({ error: "InvalidToken" }));
+    }
+  }).catch((error) => {
+    console.log(error);
+    res.status(500).end(JSON.stringify({ error: "ServerError", message: error }));
+  })
 });
 
 module.exports = router;
