d9f36c0ebdc87f3d5de064e94c73efba02577d5b
SniperFox213 <dj.gleb@list.ru>
2020-07-25 12:32:20 +0300

    Added Tunnels API Started testing Tunnels API

---
+++ tunnels/index.js
@@ -0,0 +1,37 @@
+const app        = require('express')();
+const bodyParser = require('body-parser');
+const path       = require('path');
+
+const helpers    = {
+  walk: require('./helpers/walk')
+};
+
+let { PORT } = process.env;
+
+app.use(bodyParser.json());
+
+// And now let's load all our routes...
+helpers.walk('./routes', (error, files) => {
+  files.forEach((element) => {
+    let route = require(element);
+    let filePath = path.relative(__dirname + '/routes', element);
+    let url = filePath.split('.').shift();
+
+    if (!url.includes("_")) {
+      try {
+        if (url.includes("index")) {
+          url = path.dirname(filePath);
+        };
+
+        app.use(`/${url == "." ? "" : url}`, route)
+      }
+
+      // Lock at this! It's a very complicated logging system!1!!
+      catch(error) {
+        console.log(error);
+      }
+    }
+  })
+});
+
+app.listen(PORT = 3000);
