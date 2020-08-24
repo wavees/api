829dfcb1d1d02f2eafa8baae2b7975e2559fc223
SniperFox213 <dj.gleb@list.ru>
2020-08-24 15:18:25 +0300

    ideas: Починил Socket API

--- ideas/socket/actions/getData/index.js
+++ ideas/socket/actions/getData/index.js
@@ -5,14 +5,14 @@ module.exports = (data) => {
     // Let's now check this data type.
     if (data.type == "organizationsList") {
       // (using Axios for caching)
-      axios.get('/v1/organizations', { headers: {
+      axios.get('https://api.ideas.wavees.ml/v1/organizations', { headers: {
         "Authorization": `Bearer ${data.token}`
       }})
       .then((response) => {
         let data = response.data;
 
         resolve({ dataType: "organizationsList", response: data });
-      }).catch((error) => {
+      }).catch(() => {
         resolve({ dataType: "organizationsList", error: error.response.data });
       });
     };
